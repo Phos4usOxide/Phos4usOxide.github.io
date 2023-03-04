a5e741a36f6601a944b1c9e8121758f20602e816
Julian <pokeroller123@bell.net>
2023-03-04 17:25:07 -0500

    Lab 2 done
    This commit includes some slight modifications to previous stuff.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function goToPage(page = "index") {
     //list of valid pages (not including the ".html" file ending)
-    let validPages = ["index", "about"];
+    let validPages = ["index", "about", "lab2"];
     //go to the passed page as long as it's a valid page
     if (validPages.includes(page)) {
         window.location.href = `${page}.html`;//add ".html" file ending
