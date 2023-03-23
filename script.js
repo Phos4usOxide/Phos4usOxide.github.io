f62b88332e3da38dd89e13fdfcd3da3d86df627e
Julian <pokeroller123@bell.net>
2023-03-23 01:52:28 -0400

    Improvements and modifications

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //used to redirect to another valid page
 function goToPage(page = "index") {
     //list of valid pages (not including the ".html" file ending)
-    let validPages = ["index", "about", "lab2", "lab3", "lab4"];
+    let validPages = ["index", "about", "lab1", "lab2", "lab3", "lab4", "lab5"];
     //go to the passed page as long as it's a valid page
     if (validPages.includes(page)) {
         window.location.href = `${page}.html`;//add ".html" file ending
